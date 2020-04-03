8a22501e3111a64d6dd2b5e5f922a22c4858a826
mxylovezh <18322276773@163.com>
2020-04-03 13:56:52 +0800

    我的笔记网站初始化完成

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,48 +1,97 @@
 module.exports = {
-    title: '裂泉首页', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
-    description: '裂泉的前端记录', // meta 中的描述文字，用于SEO
+    title: 'Learning', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
+    description: 'Chauncey 的的学习记录', // meta 中的描述文字，用于SEO
     // 注入到当前页面的 HTML <head> 中的标签
     head: [
-        ['link', { rel: 'icon', href: '/egg.png' }],  //浏览器的标签栏的网页图标
+        ['link', { rel: 'icon', href: '/logo.png' }],  //浏览器的标签栏的网页图标
     ],
     markdown: {
         lineNumbers: true
     },
     serviceWorker: true,
     themeConfig: {    
-        logo: '/egg.png',
+        logo: '/logo.png',
         lastUpdated: 'lastUpdate', // string | boolean
         nav: [
             { text: '首页', link: '/' },
-            {
-                text: '分类',
-                ariaLabel: '分类',
-                items: [
-                    { text: '文章', link: '/pages/folder1/test1.md' },
-                    { text: '琐碎', link: '/pages/folder2/test4.md' },
-                ]
-            },
-            { text: '功能演示', link: '/pages/folder1/test3.md' },
-            { text: 'Github', link: 'https://github.com/dwanda' },
+            { text: '基础笔记', link: '/pages/base-notes/js-this.md' },
+            { text: '项目', link: '/pages/project-notes/wc-money.md' },
+            { text: 'Node', link: '/pages/node-notes/test.md' },
+            { text: '面试题', link: '/pages/interview-notes/js-questions.md' },
+            { text: '工具', link: '/pages/tool/tool-use.md' },
+            { text: '关于我', link: '/pages/about-me/about-me.md' },
+            { text: 'Github', link: 'https://github.com/chaunceym' },
         ],
         sidebar: {
-            '/pages/folder1/':[
+            '/pages/base-notes/':[
                 {
-                    title: '测试菜单1',   // 必要的
+                    title: 'JS',   // 必要的
                     collapsable: false, // 可选的, 默认值是 true,
                     sidebarDepth: 1,    // 可选的, 默认值是 1
                     children: [
-                        ['test1.md', '子菜单1'],
-                        ['test3.md', '子菜单2']
+                        ['js-this.md', 'JS三座大山之this'],
+                        ['js-ajax.md', 'JS三座大山之ajax']
                     ]
                 },
                 {
-                    title: '测试菜单2',
+                    title: 'HTML',
                     collapsable: false, // 可选的, 默认值是 true,
                     children: [
-                        ['test2.md', '子菜单1']
+                        ['html-base.md', 'base标签的运用']
                     ]
-                }
+                },
+                {
+                    title: 'CSS',
+                    collapsable: false, // 可选的, 默认值是 true,
+                    children: [
+                        ['css-flex.md', '弹性盒子']
+                    ]
+                },
+            ],
+            '/pages/project-notes/':[
+                {
+                    title: '旺财记账',   // 必要的
+                    collapsable: false, // 可选的, 默认值是 true,
+                    sidebarDepth: 1,    // 可选的, 默认值是 1
+                    children: [
+                        ['wc-money.md', '旺财记账记账页面'],
+                        ['wc-statistic.md', '旺财记账统计页面']
+                    ]
+                },
+                {
+                    title: 'mySlide',
+                    collapsable: false, // 可选的, 默认值是 true,
+                    children: [
+                        ['my-index.md', 'mySlide首页']
+                    ]
+                },
+                {
+                    title: 'Point UI',
+                    collapsable: false, // 可选的, 默认值是 true,
+                    children: [
+                        ['po-layout.md', 'layout轮子']
+                    ]
+                },
+            ],
+            '/pages/node-notes/':[
+                {
+                    title: 'node笔记',   // 必要的
+                    collapsable: false, // 可选的, 默认值是 true,
+                    sidebarDepth: 1,    // 可选的, 默认值是 1
+                    children: [
+                        ['test.md', '测试'],
+                    ]
+                },
+            ],
+            '/pages/interview-notes/':[
+                {
+                    title: 'JS面试题',   // 必要的
+                    collapsable: false, // 可选的, 默认值是 true,
+                    sidebarDepth: 1,    // 可选的, 默认值是 1
+                    children: [
+                        ['js-questions.md', 'JavaScript'],
+                    ]
+                },
             ],
         }
     }
--- docs/README.md
+++ docs/README.md
@@ -1,19 +1,17 @@
 ---
 home: true
-heroImage: /egg.png
-heroText: 裂泉的前端记录
+heroText: Chauncey的学习笔记 
 tagline: 一点一滴都是进步
-actionText: 马上进入 →
-actionLink: /pages/folder1/test1.md
+actionText: 开始学习 →
+actionLink: /pages/base-notes/js-this.md
 features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+- title: 极简主义 
+  details: 喜欢简单的东西,崇尚干净的事物
+- title: 摇滚精神 
+  details: 架子鼓预备队员,喜欢摇滚精神
+- title: 开发高效 
+  details: 忠于高效率的编程,善于重构
+footer: Copyright © 2020-present Chauncey Meng 
 ---
-<!-- 
-<ClientOnly>
-  <BottomData/>
-</ClientOnly> -->
+
+
