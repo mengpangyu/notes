131a0dc4887403dad4e13dba3084da79f4ea829f
chauncey <18322276773@163.com>
2021-01-28 19:50:19 +0800

    feat: 添加ubuntu配置项

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,286 +1,245 @@
 module.exports = {
-  base: '/notes/',
-  title: 'Learning',
-  description: 'Chauncey 的的学习记录',
+  base: "/notes/",
+  title: "Learning",
+  description: "Chauncey 的的学习记录",
   head: [
-    ['link', {rel: 'icon', href: '/logo.png'}],  //浏览器的标签栏的网页图标
+    ["link", { rel: "icon", href: "/logo.png" }], //浏览器的标签栏的网页图标
   ],
   markdown: {
-    lineNumbers: true
+    lineNumbers: true,
   },
   serviceWorker: true,
   themeConfig: {
-    logo: '/logo.png',
-    lastUpdated: '最后更新于',
+    logo: "/logo.png",
+    lastUpdated: "最后更新于",
     nav: [
-      {text: '基础', link: '/pages/base-notes/js/js-this.md'},
-      {text: '项目', link: '/pages/project-notes/cat/wc-money.md'},
-      {text: 'Node', link: '/pages/node-notes/node-buffer.md'},
-      {text: 'Linux', link: '/pages/linux-notes/linux-start.md'},
-      {text: '算法', link: '/pages/suanfa-notes/suanfa/sort.md'},
-      {text: '技巧', link: '/pages/interview-notes/js-questions.md'},
-      {text: '工具', link: '/pages/tool-notes/tool-use.md'},
-      {text: '关于我', link: '/pages/about-me/about-me.md'},
-      {text: 'Github', link: 'https://github.com/chaunceym'},
+      { text: "基础", link: "/pages/base-notes/js/js-this.md" },
+      { text: "项目", link: "/pages/project-notes/cat/wc-money.md" },
+      { text: "Node", link: "/pages/node-notes/node-buffer.md" },
+      { text: "Linux", link: "/pages/linux-notes/linux-start.md" },
+      { text: "算法", link: "/pages/suanfa-notes/suanfa/sort.md" },
+      { text: "技巧", link: "/pages/interview-notes/js-questions.md" },
+      { text: "工具", link: "/pages/tool-notes/tool-use.md" },
+      { text: "关于我", link: "/pages/about-me/about-me.md" },
+      { text: "Github", link: "https://github.com/chaunceym" },
     ],
     sidebar: {
-      '/pages/suanfa-notes/': [
+      "/pages/suanfa-notes/": [
         {
-          title: '算法',
+          title: "算法",
           collapsable: false,
           sidebarDepth: 1,
-          children: [
-            ['suanfa/sort.md', 'Hello, 排序'],
-          ]
+          children: [["suanfa/sort.md", "Hello, 排序"]],
         },
         {
-          title: '数据结构',
+          title: "数据结构",
           collapsable: false,
           sidebarDepth: 1,
           children: [
-            ['structure/suanfa-day01.md', 'Hello, 数组'],
-            ['structure/chart01.md', 'Hello, 图'],
-            ['structure/linked01.md', 'Hello, 链表'],
-            ['structure/hash01.md', 'Hello, 哈希表'],
-            ['structure/list01.md', 'Hello, 列表'],
-            ['structure/pointer01.md', 'Hello, 双指针'],
-            ['structure/stack01.md', 'Hello, 栈'],
-            ['structure/queue01.md', 'Hello, 队列'],
-            ['structure/tree01.md', 'Hello, 二叉树']
-          ]
+            ["structure/suanfa-day01.md", "Hello, 数组"],
+            ["structure/chart01.md", "Hello, 图"],
+            ["structure/linked01.md", "Hello, 链表"],
+            ["structure/hash01.md", "Hello, 哈希表"],
+            ["structure/list01.md", "Hello, 列表"],
+            ["structure/pointer01.md", "Hello, 双指针"],
+            ["structure/stack01.md", "Hello, 栈"],
+            ["structure/queue01.md", "Hello, 队列"],
+            ["structure/tree01.md", "Hello, 二叉树"],
+          ],
         },
       ],
-      '/pages/base-notes/': [
+      "/pages/base-notes/": [
         {
-          title: 'JS',
+          title: "JS",
           collapsable: false,
           sidebarDepth: 1,
           children: [
-            ['js/js-this.md', 'JS三座大山之this'],
-            ['js/js-ajax.md', 'JS三座大山之ajax'],
-            ['js/js-promise.md', 'Promise'],
-          ]
+            ["js/js-this.md", "JS三座大山之this"],
+            ["js/js-ajax.md", "JS三座大山之ajax"],
+            ["js/js-promise.md", "Promise"],
+          ],
         },
         {
-          title: 'React',
+          title: "React",
           collapsable: false,
           children: [
-            ['react/re-class.md', 'class组件'],
-            ['react/re-function.md', '函数组件'],
-            ['react/re-hooks1.md', 'Hooks原理解析'],
-            ['react/re-hooks2.md', 'Hooks各个击破'],
-          ]
+            ["react/re-class.md", "class组件"],
+            ["react/re-function.md", "函数组件"],
+            ["react/re-hooks1.md", "Hooks原理解析"],
+            ["react/re-hooks2.md", "Hooks各个击破"],
+          ],
         },
         {
-          title: 'Vue',
+          title: "Vue",
           collapsable: false,
           children: [
-            ['vue/vue-form.md', '表单'],
-            ['vue/vue-animation.md', '深入动画原理'],
-          ]
+            ["vue/vue-form.md", "表单"],
+            ["vue/vue-animation.md", "深入动画原理"],
+          ],
         },
         {
-          title: 'TS',
+          title: "TS",
           collapsable: false,
-          children: [
-            ['ts/ts-start.md', 'TS初体验']
-          ]
+          children: [["ts/ts-start.md", "TS初体验"]],
         },
         {
-          title: 'HTML',
+          title: "HTML",
           collapsable: false,
-          children: [
-            ['html/html-base.md', 'base标签的运用']
-          ]
+          children: [["html/html-base.md", "base标签的运用"]],
         },
         {
-          title: 'CSS',
+          title: "CSS",
           collapsable: false,
-          children: [
-            ['css/css-flex.md', '弹性盒子']
-          ]
+          children: [["css/css-flex.md", "弹性盒子"]],
         },
       ],
-      '/pages/project-notes/': [
+      "/pages/project-notes/": [
         {
-          title: '小猫记账',
+          title: "小猫记账",
           collapsable: false,
           sidebarDepth: 1,
           children: [
-            ['cat/wc-money.md', '记账页面'],
-            ['cat/wc-statistic.md', '统计页面'],
-            ['cat/wc-label.md', '标签页面'],
-          ]
+            ["cat/wc-money.md", "记账页面"],
+            ["cat/wc-statistic.md", "统计页面"],
+            ["cat/wc-label.md", "标签页面"],
+          ],
         },
         {
-          title: 'mySlide',
+          title: "mySlide",
           collapsable: false,
-          children: [
-            ['myslide/my-index.md', 'mySlide介绍']
-          ]
+          children: [["myslide/my-index.md", "mySlide介绍"]],
         },
         {
-          title: 'Point UI',
+          title: "Point UI",
           collapsable: false,
-          children: [
-            ['pointui/po-index.md', 'Point UI介绍']
-          ]
+          children: [["pointui/po-index.md", "Point UI介绍"]],
         },
         {
-          title: '土豆炖牛肉',
+          title: "土豆炖牛肉",
           collapsable: false,
-          children: [
-            ['clock/clock-index.md', '土豆炖牛肉介绍']
-          ]
+          children: [["clock/clock-index.md", "土豆炖牛肉介绍"]],
         },
       ],
-      '/pages/node-notes/': [
+      "/pages/node-notes/": [
         {
-          title: 'node笔记',
+          title: "node笔记",
           collapsable: false,
           sidebarDepth: 1,
           children: [
-            ['node-buffer.md', 'Node缓冲区'],
-            ['node-callback.md', 'Node回调函数'],
-            ['node-event-loop.md', 'Node事件循环'],
-            ['node-emitter.md', 'Node事件触发器'],
-            ['node-global.md', 'Node全局对象'],
-            ['express-start.md', 'Express初探'],
-          ]
+            ["node-buffer.md", "Node缓冲区"],
+            ["node-callback.md", "Node回调函数"],
+            ["node-event-loop.md", "Node事件循环"],
+            ["node-emitter.md", "Node事件触发器"],
+            ["node-global.md", "Node全局对象"],
+            ["express-start.md", "Express初探"],
+          ],
         },
       ],
-      '/pages/linux-notes/': [
+      "/pages/linux-notes/": [
         {
-          title: 'Linux笔记',
+          title: "Linux笔记",
           collapsable: false,
           sidebarDepth: 1,
           children: [
-            ['linux-start.md', 'linux系统启动过程'],
-            ['linux-files.md', 'linux系统目录结构'],
-            ['linux-files-attr.md', 'linux文件基本属性'],
-            ['linux-files-manager.md', 'linux文件和目录管理'],
-            ['linux-disk-manager.md', 'linux磁盘管理'],
-            ['linux-shell.md', 'Shell编程'],
-            ['linux-bash.md', 'bash命令详解'],
-          ]
+            ["linux-start.md", "linux系统启动过程"],
+            ["linux-files.md", "linux系统目录结构"],
+            ["linux-files-attr.md", "linux文件基本属性"],
+            ["linux-files-manager.md", "linux文件和目录管理"],
+            ["linux-disk-manager.md", "linux磁盘管理"],
+            ["linux-shell.md", "Shell编程"],
+            ["linux-bash.md", "bash命令详解"],
+            ["linux-config.md", "我的 Ubuntu"],
+          ],
         },
       ],
-      '/pages/interview-notes/': [
+      "/pages/interview-notes/": [
         {
-          title: 'JS技巧',
+          title: "JS技巧",
           collapsable: true,
           sidebarDepth: 1,
-          children: [
-            ['js-questions.md', 'JavaScript'],
-          ]
+          children: [["js-questions.md", "JavaScript"]],
         },
         {
-          title: 'Vue技巧',
+          title: "Vue技巧",
           collapsable: true,
           sidebarDepth: 1,
-          children: [
-            ['vue-questions.md', 'Vue'],
-          ]
+          children: [["vue-questions.md", "Vue"]],
         },
         {
-          title: 'React技巧',
+          title: "React技巧",
           collapsable: true,
           sidebarDepth: 1,
-          children: [
-            ['react-questions.md', 'React'],
-          ]
+          children: [["react-questions.md", "React"]],
         },
         {
-          title: 'Dom技巧',
+          title: "Dom技巧",
           collapsable: true,
           sidebarDepth: 1,
-          children: [
-            ['dom-questions.md', 'dom'],
-          ]
+          children: [["dom-questions.md", "dom"]],
         },
         {
-          title: 'Http技巧',
+          title: "Http技巧",
           collapsable: true,
           sidebarDepth: 1,
-          children: [
-            ['http-questions.md', 'Http'],
-          ]
+          children: [["http-questions.md", "Http"]],
         },
         {
-          title: 'Css技巧',
+          title: "Css技巧",
           collapsable: true,
           sidebarDepth: 1,
-          children: [
-            ['css-questions.md', 'Css'],
-          ]
+          children: [["css-questions.md", "Css"]],
         },
         {
-          title: 'Html技巧',
+          title: "Html技巧",
           collapsable: true,
           sidebarDepth: 1,
-          children: [
-            ['html-questions.md', 'Html'],
-          ]
+          children: [["html-questions.md", "Html"]],
         },
         {
-          title: 'Webpack技巧',
+          title: "Webpack技巧",
           collapsable: true,
           sidebarDepth: 1,
-          children: [
-            ['webpack-questions.md', 'Webpack'],
-          ]
+          children: [["webpack-questions.md", "Webpack"]],
         },
         {
-          title: 'TypeScript技巧',
+          title: "TypeScript技巧",
           collapsable: true,
           sidebarDepth: 1,
-          children: [
-            ['ts-questions.md', 'TypeScript'],
-          ]
+          children: [["ts-questions.md", "TypeScript"]],
         },
         {
-          title: 'Node技巧',
+          title: "Node技巧",
           collapsable: true,
           sidebarDepth: 1,
-          children: [
-            ['node-questions.md', 'Node'],
-          ]
+          children: [["node-questions.md", "Node"]],
         },
         {
-          title: '开放题和刁钻题',
+          title: "开放题和刁钻题",
           collapsable: true,
           sidebarDepth: 1,
-          children: [
-            ['open-questions.md', 'Other'],
-          ]
+          children: [["open-questions.md", "Other"]],
         },
       ],
-      '/pages/tool-notes/': [
+      "/pages/tool-notes/": [
         {
-          title: '常用工具',
+          title: "常用工具",
           collapsable: false,
           sidebarDepth: 1,
-          children: [
-            ['tool-use.md', '工具'],
-          ]
+          children: [["tool-use.md", "工具"]],
         },
         {
-          title: 'webpack',
+          title: "webpack",
           collapsable: false,
           sidebarDepth: 1,
-          children: [
-            ['webpack-use.md', 'webpack'],
-          ]
+          children: [["webpack-use.md", "webpack"]],
         },
         {
-          title: 'parcel',
+          title: "parcel",
           collapsable: false,
           sidebarDepth: 1,
-          children: [
-            ['parcel-use.md', 'parcel'],
-          ]
+          children: [["parcel-use.md", "parcel"]],
         },
       ],
-    }
-  }
-}
+    },
+  },
+};
---
+++ docs/pages/linux-notes/linux-config.md
@@ -0,0 +1,94 @@
+# Ubuntu 18.04 常用软件
+
+## JDK
+
+因个人原因需要安装 JKD8, 在这里安装
+
+```bash
+sudo apt update
+
+sudo apt install openjdk-8-jdk
+
+java -version
+```
+
+## Node.js
+
+下载 12.x 版本
+
+```bash
+cd ~
+curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+
+sudo apt update
+
+sudo apt install nodejs
+```
+
+卸载
+
+```bash
+sudo apt remove nodejs
+
+sudo apt purge nodejs
+
+sudo apt autoremove
+```
+
+## Android Studio
+
+[中文 Android Studio 社区](http://www.android-studio.org/)
+
+下载好 Android Studio 后根据 [React Native 官网配置](https://reactnative.cn/docs/environment-setup) 好 SDK
+
+## VSCode
+
+[vscode 官网](https://code.visualstudio.com/)
+
+## Webstrom
+
+[webstrom 官网](https://www.jetbrains.com/webstorm/)
+
+下载解压后运行 bin/webstrom.sh
+
+## Fira Code
+
+```bash
+sudo apt install fonts-firacode
+```
+
+## Charles
+
+```bash
+wget -q -O - https://www.charlesproxy.com/packages/apt/PublicKey | sudo apt-key add
+
+sudo sh -c 'echo deb https://www.charlesproxy.com/packages/apt/ charles-proxy main > /etc/apt/sources.list.d/charles.list'
+
+sudo apt-get update
+
+sudo apt-get install charles-proxy
+```
+
+## Chrome
+
+[官网下载](https://www.google.com/intl/en_hk/chrome/)
+
+## utools
+
+[官网下载](https://u.tools/)
+
+## WPS
+
+[官网下载](https://linux.wps.com/)
+
+## Ubuntu 美化
+
+[Linux 玩家必备：Ubuntu 完全配置指南](https://zhuanlan.zhihu.com/p/56253982)
+
+[[18.04/美化] 总结一下 Ubuntu 安装完之后都会做的事情](https://zhuanlan.zhihu.com/p/36200924)
+
+[下载下拉终端插件需要完成美化后](https://ywnz.com/linuxjc/5735.html)
+
+## 杂项
+
+命令行安装 vim(使用方方的配置), curl, netstat
